9493c633c05250edb98666b336e446db95d7e38e
suryanarayanan21 <suryanarayanan.kviisc@gmail.com>
2025-11-17 22:46:18 +0000

    Setup react router

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "../data/store.ts";
-import App from "./App.tsx";
+import { RouterProvider } from "react-router/dom";
+import router from "./Router.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>
 );
