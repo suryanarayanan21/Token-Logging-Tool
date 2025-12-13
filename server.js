9b4e4be00d7c39906652a3b6ecd137eeebdf6318
suryanarayanan21 <suryanarayanan.kviisc@gmail.com>
2025-12-13 20:46:51 +0000

    Added changes to deploy correctly

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import multer from "multer";
 const app = express();
 const upload = multer({storage: multer.memoryStorage()});
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use("/", express.static("dist"));
 app.use(express.json());
